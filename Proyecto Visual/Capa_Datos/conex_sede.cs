6bd382e0d3d2f1bb607942f33ff214f4ca5ff456
Braulio Castillo <braulioc6@gmail.com>
2020-08-31 19:56:18 -0600

    conex sede y super
    Clase conexión de súper y sede para ingresar y modificar

---
+++ Proyecto Visual/Capa_Datos/conex_sede.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Datos
+{
+    class conex_sede
+    {
+
+        public SqlCommand cmd;
+        public SqlConnection cnx;
+        SqlDataAdapter adaptador;
+        SqlDataReader dataReader;
+        public string conection = "";
+
+        #region metodos
+
+        #region ingresa sede
+
+        public void ingresaSede(int id, string localidad, int idEncargado, int idSuper)
+        {
+
+            try
+            {
+                if (validaSede(id) == false)
+                {
+                    cnx = new SqlConnection(conection);
+                    cnx.Open();
+                    cmd = new SqlCommand("execute creaSede " + id + ", '" + localidad + "', " + idEncargado + ", " + idSuper, cnx);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Sede agregada.");
+                    cnx.Close();
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Hubo un error al agregar la sede, por favor inténtelo nuevamente");
+            }
+
+
+        }
+
+        private bool validaSede(int id)
+        {
+            bool valida = false;
+
+            try
+            {
+                cnx = new SqlConnection(conection);
+                cnx.Open();
+                cmd = new SqlCommand("select * from Sede", cnx);
+                cmd.ExecuteNonQuery();
+                dataReader = cmd.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    if (dataReader["ID"].ToString().Equals(id))
+                    {
+                        MessageBox.Show("La sede ya existe");
+                        return true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Ha ocurrido un error, por favor inténtelo nuevamente");
+            }
+
+                return valida;
+            }
+
+        #endregion
+
+        #region modifica sede
+
+        public void modificaSede(int id, string nombre, string localidad)
+        {
+
+
+            try
+            {
+                cnx = new SqlConnection(conection);
+                cnx.Open();
+                cmd = new SqlCommand("execute modificaSede " + id + ", '" + nombre + "', '" + localidad + "'", cnx);
+                cmd.ExecuteNonQuery();
+                cnx.Close();
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error al modificar la sede");
+            }
+
+        }
+
+        #endregion
+
+
+
+        #endregion
+    }
+}
---
+++ Proyecto Visual/Capa_Datos/conex_super.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Datos
+{
+    class conex_super
+    {
+
+        public SqlCommand cmd;
+        public SqlConnection cnx;
+        SqlDataAdapter adaptador;
+        SqlDataReader dataReader;
+        public string conection = "";
+
+
+        #region metodos
+
+        #region ingresaSuper
+        public void ingresaSuper(int id, string nombre, string localidad, int IDDUeño)
+        {
+            try
+            {
+                if (validaIDSuper(id) == false)
+                {
+                    cnx = new SqlConnection(conection);
+                    cnx.Open();
+                    cmd = new SqlCommand("execute insertaSuper " + id + ", '" + nombre + "', '" + localidad + "', " + IDDUeño, cnx);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Súpermercado agregado.");
+                    cnx.Close();
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Hubo un error al ingresar el súpermercado, por favor intente nuevamente");
+            }
+
+
+        }
+
+        private bool validaIDSuper(int id)
+        {
+            bool valida = false;
+
+            cnx = new SqlConnection(conection);
+            cnx.Open();
+            cmd = new SqlCommand("select * from Supermercado" , cnx);
+            cmd.ExecuteNonQuery();
+            dataReader = cmd.ExecuteReader();
+            try
+            {
+                while (dataReader.Read())
+                {
+                    if (dataReader["ID"].ToString().Equals(id))
+                    {
+                        MessageBox.Show("El súpermercado ya existe");
+                        return true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Ha ocurrido un error, por favor inténtelo nuevamente");
+            }
+            cnx.Close();
+            return valida;
+        }
+
+        #endregion
+
+
+        #region modificaSuper
+        public void modificaSuper(int id, string nombre, string localidad, string IDDueño)
+        {
+
+            
+            try
+            {
+                cnx = new SqlConnection(conection);
+                cnx.Open();
+                cmd = new SqlCommand("execute modificaSuper " + id + ", '" + nombre + "', '" + localidad + "', " + IDDueño, cnx);
+                cmd.ExecuteNonQuery();
+                cnx.Close();
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error al modificar el súpermercado");
+            }
+
+        }
+        #endregion
+
+        #endregion
+    }
+}
